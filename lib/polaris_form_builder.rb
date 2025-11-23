6b870517fdae28e320b6379196dfc7d451f0f807
Lex Cao <lexcao@foxmail.com>
2025-11-24 02:17:52 +0800

    bundle gem

---
+++ lib/polaris_form_builder.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "polaris_form_builder/version"
+
+module PolarisFormBuilder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_polaris_form_builder.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestPolarisFormBuilder < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::PolarisFormBuilder::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
