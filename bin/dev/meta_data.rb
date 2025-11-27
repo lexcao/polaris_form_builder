73b6b6be34833c4dee0bdb92dcdf2b0e30ce7196
Lex Cao <lexcao@foxmail.com>
2025-11-28 02:47:46 +0800

    feat(meta_data): fetch all Markdown

--- bin/dev/meta_data.rb
+++ bin/dev/meta_data.rb
@@ -6,15 +6,21 @@ require_relative 'fetch'
 
 module MetaData
   BASE_URL = "https://shopify.dev/docs/api/app-home/polaris-web-components/forms/"
-  Result = Data.define(:key, :name, :url)
+  Result = Data.define(:key, :name, :markdown_content, :markdown_url)
 
   COMPONENTS = %w[checkbox choicelist colorfield colorpicker datefield datepicker dropzone emailfield moneyfield numberfield passwordfield searchfield select switch textarea textfield urlfield]
-  DATA = COMPONENTS.map { |component| Result.new(key: component, name: component.camelcase, url: "#{BASE_URL}#{component}") }
+  DATA = COMPONENTS.map { |component| Result.new(key: component, name: component.camelcase, markdown_url: "#{BASE_URL}#{component}.md") }
   private_constant :DATA
 
   module_function
 
   def list
     DATA
+  end
+
+  def fetch_all
+    DATA.map do |component|
+      component.with(markdown_content: Fetch.with_cache(component.markdown_url))
+    end
   end
 end
