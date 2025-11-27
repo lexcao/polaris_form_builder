cee0f39803582feac4d76ba90394efdfe9337213
Lex Cao <lexcao@foxmail.com>
2025-11-28 01:06:26 +0800

    feat(fetch): add fetch

---
+++ bin/dev/fetch.rb
@@ -0,0 +1,29 @@
+require "digest"
+require "net/http"
+require "uri"
+
+def fetch(url)
+  uri = URI(url)
+  Net::HTTP.get(uri)
+end
+
+module Fetch
+  CACHE_DIR = File.expand_path("tmp", __dir__)
+
+  module_function
+
+  def with_cache(url)
+    file_path = cache_file_path(url)
+    return File.read(file_path) if File.exist?(file_path)
+
+    fetch(url).tap { |it| File.write(file_path, it) }
+  end
+
+  def cache_file_path(url)
+    File.join(CACHE_DIR, cache_key(url))
+  end
+
+  def cache_key(url)
+    "cache_" + Digest::SHA256.hexdigest(url)
+  end
+end
---
+++ test/dev/fetch_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+
+require_relative '../../bin/dev/fetch'
+
+class FetchTest < Minitest::Test
+  def setup
+    @url = "https://shopify.dev/docs/api/app-home/polaris-web-components/forms/textfield"
+  end
+
+  def test_fetch
+    refute_empty fetch(@url)
+  end
+
+  def test_fetch_with_cache
+    cache_file_path = Fetch.cache_file_path(@url)
+
+    refute File.exist?(cache_file_path)
+    body = Fetch.with_cache(@url)
+    refute_empty body
+    assert File.exist?(cache_file_path)
+  ensure
+    File.delete(cache_file_path)
+  end
+end
