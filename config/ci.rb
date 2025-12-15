18ff9c111298672adf345a85b84151a3ed7ef2ed
Lex Cao <lexcao@foxmail.com>
2025-12-15 23:32:48 +0800

    ci: run bundle steps in unbundled env

--- config/ci.rb
+++ config/ci.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
-CI.run do
-  step 'Bundle: root', 'bundle install'
-  step 'Bundle: dummy', 'env BUNDLE_GEMFILE=test/dummy/Gemfile bundle install'
-  step 'Bundle: playground', 'env BUNDLE_GEMFILE=app/playground/Gemfile bundle install'
+def unbundled(command)
+  env_unset = %w[
+    BUNDLE_BIN_PATH
+    BUNDLE_GEMFILE
+    BUNDLER_VERSION
+    GEM_HOME
+    GEM_PATH
+    RUBYOPT
+  ].map { |key| "-u #{key}" }.join(' ')
 
-  step 'Style: Ruby', 'bin/rubocop -f github'
-  step 'Tests', 'bundle exec rake test'
+  "env #{env_unset} #{command}"
+end
+
+CI.run do
+  step 'Bundle: root', unbundled('bundle install --jobs 4 --retry 3')
+  step 'Bundle: dummy', unbundled('bundle install --jobs 4 --retry 3 --gemfile test/dummy/Gemfile')
+  step 'Bundle: playground', unbundled('bundle install --jobs 4 --retry 3 --gemfile app/playground/Gemfile')
+
+  step 'Style: Ruby', unbundled('bin/rubocop -f github')
+  step 'Tests', unbundled('bundle exec rake test')
 
   failure 'Signoff: CI failed.', 'Fix the issues and try again.' unless success?
 end
