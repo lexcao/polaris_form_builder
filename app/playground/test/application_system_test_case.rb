5175b485562542b1f271a6abc408d7e82f541e7c
Lex Cao <lexcao@foxmail.com>
2025-12-16 22:43:00 +0800

    test(playground): add preview system test

---
+++ app/playground/test/application_system_test_case.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "capybara/shadowdom"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  parallelize(workers: 1)
+
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1000]
+end
---
+++ app/playground/test/system/text_field_preview_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class TextFieldPreviewTest < ApplicationSystemTestCase
+  test "preview submits and renders Result for TextField" do
+    visit component_path("textfield")
+
+    assert_selector "h3", text: "Preview"
+    assert_selector "s-text-field[name='preview[store_name]']", visible: :all
+    assert_selector "form[action$='/preview']", visible: :all
+
+    fill_in_preview_text_field(name: "preview[store_name]", with: "ACME Store")
+    submit_preview
+
+    assert_selector "h3", text: "Result"
+    assert_text "\"store_name\": \"ACME Store\""
+
+    visit current_path
+    assert_no_selector "h3", text: "Result"
+  end
+
+  private
+    def fill_in_preview_text_field(name:, with:)
+      host = preview_form.find("s-text-field[name='#{name}']", match: :first, visible: :all)
+
+      within(host.shadow_root) do
+        input = find("input, textarea", visible: :all)
+        input.click
+        input.send_keys([:command, "a"], :backspace)
+        input.send_keys([:control, "a"], :backspace)
+        input.send_keys(with)
+      end
+    end
+
+    def submit_preview
+      host = preview_form.find("s-button", text: "Try it", exact_text: true)
+
+      within(host.shadow_root) do
+        find("button, input[type='submit']", visible: :all).send_keys(:enter)
+      end
+    end
+
+    def preview_form
+      find("s-button", text: "Try it", exact_text: true).ancestor("form", visible: :all)
+    end
+end
