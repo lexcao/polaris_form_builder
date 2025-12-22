b51a681191216a99509f4bff6bea275ae86e832e
Lex Cao <lexcao@foxmail.com>
2025-12-22 22:59:18 +0800

    chore(playground): remove Turbo debug logging

--- app/playground/app/javascript/application.js
+++ app/playground/app/javascript/application.js
@@ -1,33 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 import "@hotwired/turbo-rails"
-
-if (new URLSearchParams(window.location.search).has("debug_turbo")) {
-  const log = (...args) => console.log("[turbo-debug]", ...args)
-
-  const events = [
-    "turbo:click",
-    "turbo:before-visit",
-    "turbo:visit",
-    "turbo:before-fetch-request",
-    "turbo:before-fetch-response",
-    "turbo:before-cache",
-    "turbo:before-render",
-    "turbo:render",
-    "turbo:load",
-  ]
-
-  for (const eventName of events) {
-    document.addEventListener(eventName, (event) => {
-      const detail = event?.detail || {}
-      const url = detail.url?.toString?.() || detail.url || null
-
-      log(eventName, {
-        url,
-        action: detail.action,
-        fetchOptions: detail.fetchOptions,
-        responseStatus: detail.fetchResponse?.response?.status,
-      })
-    })
-  }
-}
