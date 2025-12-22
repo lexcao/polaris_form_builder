eeaa3ad19d2dd759951d77c821a9b006d2f9a698
Lex Cao <lexcao@foxmail.com>
2025-12-22 22:41:36 +0800

    feat(playground): Polaris components home layout

--- app/playground/app/helpers/components_helper.rb
+++ app/playground/app/helpers/components_helper.rb
@@ -1,2 +1,11 @@
 module ComponentsHelper
+  def component_screenshot_url(component)
+    slug =
+      case component.name.to_s
+      when "ColorField" then "color-field"
+      when "ColorPicker" then "color-picker"
+      else component.name.to_s.downcase
+      end
+    "https://shopify.dev/images/templated-apis-screenshots/admin/components/#{slug}.png"
+  end
 end
