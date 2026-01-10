9a8ff9956bdec61db9d372f732a66129d4730dbc
Lex Cao <lexcao@foxmail.com>
2026-01-10 14:54:58 +0800

    test(tag): add tests

--- test/tag_test.rb
+++ test/tag_test.rb
@@ -25,4 +25,39 @@ class TagTest < Minitest::Test
     expect = "<s-text-field type='text'><s-icon slot='accessory' interestfor='info-tooltip'></s-icon></s-text-field>"
     assert_dom_equal expect, actual
   end
+
+  def test_apply_with_attr
+    tag = PolarisFormBuilder::Tag.new("s-text-field", "input")
+                                 .attr("value", "test")
+
+    actual = tag.apply("<input type='text'/>")
+
+    expect = "<s-text-field type='text' value='test'></s-text-field>"
+    assert_dom_equal expect, actual
+  end
+
+  def test_apply_attr_to_child
+    tag = PolarisFormBuilder::Tag.new("s-button", "input")
+                                 .attr_to_child("value")
+
+    actual = tag.apply("<input type='submit' value='Submit'/>")
+
+    expect = "<s-button type='submit' value='Submit'>Submit</s-button>"
+    assert_dom_equal expect, actual
+  end
+
+  def test_apply_child_to_attr
+    tag = PolarisFormBuilder::Tag.new("s-text-area", "textarea")
+                                 .child_to_attr("value")
+
+    given = <<~HTML
+      <textarea>Ruby on Rails
+      Shopify Polaris Web Components</textarea>
+    HTML
+
+    actual = tag.apply(given)
+
+    expect = "<s-text-area value='Ruby on Rails\nShopify Polaris Web Components'></s-button>"
+    assert_dom_equal expect, actual
+  end
 end
