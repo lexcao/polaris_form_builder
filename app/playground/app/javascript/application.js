a0e8bdf359cc49ad023b3157a9b5459d20d9f92e
Lex Cao <lexcao@foxmail.com>
2026-01-14 22:03:32 +0800

    fix(playground): rerun Prism after Turbo morph

--- app/playground/app/javascript/application.js
+++ app/playground/app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 import "@hotwired/turbo-rails"
+
+const highlightPrism = () => {
+  if (!window.Prism) return
+
+  requestAnimationFrame(() => {
+    window.Prism.highlightAll()
+  })
+}
+
+document.addEventListener("turbo:render", highlightPrism)
